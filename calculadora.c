943c7f70cecb9c56a2aa0acc255687e5dcbe7a4b
qtyuz <66525913+qtyuz@users.noreply.github.com>
2023-03-17 22:40:43 -0300

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    float base = 5;
+    float altura = 8;
+    float area = 0;
+ area = (base * altura) / 2;
+
+ printf("a area do triangulo é: %.2f", area);
+ 
+return 0;
+
+}
