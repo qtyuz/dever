f8f1e97d82009a1217160fbf87ea1e090cfb42f5
qtyuz <66525913+qtyuz@users.noreply.github.com>
2023-05-02 23:42:12 -0300

    Update atividade7.c

--- atividade7.c
+++ atividade7.c
@@ -1,15 +1,45 @@
 #include <stdio.h>
 
+int validaQuantidade(int pecas) {
+    if (pecas >= 0) {
+        return 1; 
+    } else {
+        return 0; 
+    }
+}
+float calculaSalario(int pecas) {
+    const float SALARIO_BASE = 600.0;
+    float adicional = 0.0;
+    
+    if (pecas > 50 && pecas <= 80) {
+        adicional = 0.5 * (pecas - 50);
+    } else if (pecas > 80) {
+        adicional = 0.5 * 30 + 0.75 * (pecas - 80);
+    }
+    
+    return SALARIO_BASE + adicional;
+}
 int main() {
-    float custo_fabrica, custo_consumidor;
-    float percent_distribuidor = 0.28, impostos = 0.45;
-
-    printf("Informe o custo de fabrica do carro:R$");
-    scanf("%f", &custo_fabrica);
-
-    custo_consumidor = custo_fabrica + (custo_fabrica * percent_distribuidor) + (custo_fabrica * impostos);
-
-    printf("O custo final do carro ao consumidor e:R$ %.2f\n", custo_consumidor);
-
+    int pecas;
+    float salario;
+    
+    while (1) { 
+        printf("Digite a quantidade de pecas fabricadas ou um valor negativo para sair: ");
+        scanf("%d", &pecas);
+        
+        if (pecas < 0) { 
+            break;
+        }
+        
+        if (validaQuantidade(pecas)) {
+            salario = calculaSalario(pecas);
+            printf("O salario do funcionario e: R$ %.2f\n", salario);
+        } else {
+            printf("Quantidade invalida de pecas!\n");
+        }
+    }
+    
+    printf("Fim do programa.\n");
+    
     return 0;
 }
